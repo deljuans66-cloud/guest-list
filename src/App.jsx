d178dde03543e3a06e9e3f58743e5656eb8c28b5
DelJuan Smith <deljuans66@gmail.com>
2025-11-13 19:18:47 -0500

    Eveything working as intended

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,81 @@
+import { useEffect, useState } from "react";
+import { getGuests, getGuest } from "./guest-list";
+import "./index.css";
+
 export default function App() {
-  return <></>;
+  const [guestId, setGuestId] = useState(null);
+  return (
+    <>
+      <h1>Guest List</h1>
+      <section>
+        <h2>Name</h2> <h2>Email</h2> <h2>Phone</h2>
+      </section>
+      <GuestList setGuestId={setGuestId} />
+      <GuestDetails guestId={guestId} />
+    </>
+  );
+}
+
+function GuestList({ setGuestId }) {
+  const [guests, setGuests] = useState([]);
+
+  useEffect(() => {
+    const syncGuests = async () => {
+      const data = await getGuests();
+      setGuests(data);
+    };
+    syncGuests();
+  }, []);
+
+  return (
+    <ul className="guests">
+      {guests.map((guest) => (
+        <li key={guest.id} onClick={() => setGuestId(guest.id)}>
+          <h3>
+            {guest.name} #{guest.id}
+          </h3>{" "}
+          <h3>{guest.email}</h3> <h3>{guest.phone}</h3>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function GuestDetails({ guestId }) {
+  const [guest, setGuest] = useState(null);
+
+  useEffect(() => {
+    const syncGuest = async () => {
+      if (!guestId) return;
+
+      const data = await getGuest(guestId);
+      setGuest(data);
+    };
+    syncGuest();
+  }, [guestId]);
+
+  if (!guest) {
+    return <p>Select a guest to see more details.</p>;
+  }
+
+  return (
+    <div className="guests">
+      <h2>
+        {guest.name} #{guest.id}
+      </h2>
+      <p>
+        <strong>Email:</strong> {guest.email}
+      </p>
+      <p>
+        <strong>Phone:</strong> {guest.phone}
+      </p>
+      <p>
+        <strong>Job:</strong> {guest.job}
+      </p>
+      <p>
+        <strong>Bio:</strong> {guest.bio}
+      </p>
+      <button onClick={() => setGuest(null)}>Back to list</button>
+    </div>
+  );
 }
---
+++ src/guest-list.js
@@ -0,0 +1,23 @@
+import { API } from "./constants";
+
+export async function getGuests() {
+  try {
+    const response = await fetch(API + "/guests");
+    const result = await response.json();
+    return result.data;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
+
+export async function getGuest(id) {
+  try {
+    const response = await fetch(API + "/guests/" + id);
+    const result = await response.json();
+    return result.data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
